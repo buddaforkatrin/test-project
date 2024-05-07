a5e436b260e9a9201603462e792bc6a2e7f2b169
buddaforkatrin <buddaforkatrin@gmail.com>
2024-05-07 15:12:41 +0200

    hw2.4.4 updated

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const calculateBonus = (a, b) => {
   let bonus;
   const sum = a + b;
-  debugger; // вычислилась сумма a и b
+  debugger; // forced stop of the script
   sum > 50 ? (bonus = 50) : (bonus = sum);
-  debugger; // на основе sum вычисляется бонус,макс значение бонуса 50
+  debugger; // forced stop of the script
   return bonus;
 };
 calculateBonus(2, 3);
