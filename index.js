79ec771a1b9b198cf8fe72fb95f0a03a3d1cf4ab
buddaforkatrin <buddaforkatrin@gmail.com>
2024-05-28 00:02:59 +0200

    h.w 3.1.1 unit tests added

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const calculateBonus = (a, b) => {
   let bonus;
   const sum = a + b;
-  debugger; // forced stop of the script
   sum > 50 ? (bonus = 50) : (bonus = sum);
-  debugger; // forced stop of the script
   return bonus;
 };
-calculateBonus(2, 3);
+
+module.exports = calculateBonus;
